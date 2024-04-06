c4f5ab5a8122842d1cd2abae2917bc51327c2c57
Jeff Camera <jeff@jeffcamera.com>
2024-04-06 09:18:53 -0400

    inject into existing tabs when first installed

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,22 @@
 chrome.runtime.onInstalled.addListener(
-    details => {
+    async details => {
         if (details.reason === 'install') {
+            // Open the onboarding page.
             chrome.tabs.create({
                 url: chrome.runtime.getURL('hello.html')
             });
+            // Manually inject the content script into existing tabs.
+            const ytTabs = await chrome.tabs.query({ url: 'https://*.youtube.com/*' });
+            for (const tab of ytTabs) {
+                await chrome.scripting.executeScript({
+                    target: {
+                        tabId: tab.id
+                    },
+                    files: [
+                        'content.js'
+                    ]
+                });
+            }
         }
     }
 );
